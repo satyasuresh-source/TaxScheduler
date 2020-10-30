df7f97096258bdbd56b005f3b265dca36bfeea2a
Suresh Palepu <palepusatyasuresh@gmail.com>
2020-10-30 21:57:15 +0530

    Missing Models folder added

---
+++ Models/FileAccessRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TaxScheduler.Models
+{
+   public class FileAccessRequest
+    {      
+        public string FileType { get; set; }
+     
+        public string FilePath { get; set; }
+
+    }
+}
---
+++ Models/Municipality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TaxScheduler.Models
+{
+    public class Municipality
+    {        
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Tax { get; set; }
+        public string Frequency { get; set; }
+
+    }
+}
